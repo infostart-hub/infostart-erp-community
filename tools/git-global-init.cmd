1a46af3ced55709edd248fdb3877c49b0205c034
Artur Ayukhanov <aartbear@gmail.com>
2020-06-24 15:43:11 +0300

    первонаальная настройка Гит

---
+++ tools/git-global-init.cmd
@@ -0,0 +1,24 @@
+@chcp 65001
+
+@rem for current user
+git config --global user.name "Artur Ayukhanov"
+git config --global user.email aartbear@gmail.com
+
+@rem global
+
+git config --global core.quotePath false
+
+@rem for Windows
+git config --global core.autocrlf true
+git config --global core.safecrlf true
+
+@rem for Linux and MacOS
+@rem git config --global core.autocrlf input
+@rem git config --global core.safecrlf true
+
+git config --global http.postBuffer 1048576000
+
+@echo do it only for administrator mode
+git config --system core.longpaths true
+
+SET LC_ALL=C.UTF-8
