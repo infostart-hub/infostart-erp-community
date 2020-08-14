e319f6e636406ff683b2cbcd94b4aaa349ddd76f
Artur Ayukhanov <aartbear@gmail.com>
2020-08-14 17:51:54 +0300

    Обновление из хранилища

--- build.cmd
+++ build.cmd
@@ -3,6 +3,10 @@
 @rem формирование файла конфигурации. для включения раскомментируйте код ниже
 call vrunner compile --src src/cf --out build/1cv8.cf %*
 
+@rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
+@rem call vrunner loadrepo %*
+@rem call vrunner updatedb %*
+
 @rem собрать внешние обработчики и отчеты в каталоге build
 @rem call vrunner compileepf src/epf/МояВнешняяОбработка build %*
 @rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
