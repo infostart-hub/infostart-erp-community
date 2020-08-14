1d6b6a45c523493930d060886200b697c3861527
Artur Ayukhanov <aartbear@gmail.com>
2020-08-14 17:51:28 +0300

    обновление из хранилища

--- prepare.cmd
+++ prepare.cmd
@@ -3,6 +3,10 @@
 @rem Сборка основной разработческой ИБ. по умолчанию в каталоге build/ib
 call vrunner init-dev --src src/cf %*
 
+@rem обновление конфигурации основной разработческой ИБ из хранилища. для включения раскомментируйте код ниже
+@rem call vrunner loadrepo %*
+@rem call vrunner updatedb %*
+
 @rem собрать внешние обработчики и отчеты в каталоге build
 @rem call vrunner compileepf src/epf/МояВнешняяОбработка build %*
 @rem call vrunner compileepf src/erf/МойВнешнийОтчет build %*
